6f6b09e70df7418b82d585fa160632f9ff94858c
Source-FoX <76773605+Source-FoX@users.noreply.github.com>
2020-12-31 00:18:48 +0300

    Update FoX.lua

--- FoX.lua
+++ FoX.lua
@@ -280,9 +280,9 @@ end
 return var
 end 
 function Rutba(user_id,chat_id)
-if tonumber(user_id) == tonumber(1200805273) then  
+if tonumber(user_id) == tonumber(798700870) then  
 var = 'مالك السورس'
-elseif tonumber(user_id) == tonumber(798700870) then
+elseif tonumber(user_id) == tonumber(1200805273) then
 var = 'مطور السورس'
 elseif tonumber(user_id) == tonumber(SUDO) then
 var = 'المطور الاساسي'  
